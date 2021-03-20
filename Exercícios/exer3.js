24b0abba8229ca9b97a5e069a7832fe81313c00e
GGM <ra116843@uem.br>
2021-03-20 14:21:57 -0300

    Exercício em py

--- Exercícios/exer3.js
+++
@@ -1,7 +0,0 @@
-//Dado o lado de um triângulo equilátero, imprima sua área. A = (√3/4) * lado²;
-//Entrada: 12, Saída: 62.35. 
-//Entrada: 4.5, Saída: 8.77
-const lado = 4.5
-const area = (Math.sqrt(3)/4)*lado**2
-
-console.log(area.toFixed(2))
